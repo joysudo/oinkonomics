bab407c7ae9f3440fda1b2094a937032c11ee8f5
Joy <joysu2008@gmail.com>
2026-01-15 07:43:38 -0500

    add horizontal ruler

---
+++ camera_2d.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+@export var horizontal_ruler: Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var bottom_right = get_screen_center_position() + (get_viewport_rect().size / 2 / zoom)
+	if horizontal_ruler:
+		horizontal_ruler.global_position.y = bottom_right.y - horizontal_ruler.texture.get_height()
